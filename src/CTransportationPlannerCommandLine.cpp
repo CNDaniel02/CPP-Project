f621cb17a49788f6ccddae617b9bb0b50a4e18c8
Dongmin Wu <chnwu@ucdavis.edu>
2024-03-10 20:40:43 -0700

    making progress

--- src/CTransportationPlannerCommandLine.cpp
+++ src/CTransportationPlannerCommandLine.cpp
@@ -1,15 +1,14 @@
 #include "TransportationPlannerCommandLine.h"
-#include <iostream>
-#include <sstream>
 #include <string>
+#include <sstream>
+
 
 struct CTransportationPlannerCommandLine::SImplementation {
-    std::shared_ptr<CDataSource> CmdSource;
+    std::shared_ptr<CDataSource> CommandSource;
     std::shared_ptr<CDataSink> OutSink;
-    std::shared_ptr<CDataSink> ErrSink;
+    std::shared_ptr<CDataSink> ErrorSink;
     std::shared_ptr<CDataFactory> Results;
     std::shared_ptr<CTransportationPlanner> Planner;
 
-    SImplementation(std::shared_ptr<CDataSource> cmdsrc, std::shared_ptr<CDataSink> outsink, std::shared_ptr<CDataSink> errsink, std::shared_ptr<CDataFactory> results, std::shared_ptr<CTransportationPlanner> planner)
-        : CmdSource(cmdsrc), OutSink(outsink), ErrSink(errsink), Results(results), Planner(planner) {}
+    SImplementation(std::shared_ptr<CDataSource> commandsrc,std::shared_ptr<CDataSink> outsink,std::shared_ptr<CDataSink> errorsink,std::shared_ptr<CDataFactory> results,std::shared_ptr<CTransportationPlanner> planner): CmdSource(cmdsrc), OutSink(outsink), ErrSink(errsink), Results(results), Planner(planner) {}
 };
